ea7f065a50c2fd7efb604fb30780f3e39f828c4e
jman13378 <garconiamail@gmail.com>
2023-05-05 14:04:28 -0400

    m

--- index.js
+++ index.js
@@ -1,91 +1,149 @@
-const request = require('request');
+const request = require("request");
 
-const baseURL = "https://dashboard.sokobot.info/"
-const APIPath = "api/"
-
+const baseURL = "https://dashboard.sokobot.info/";
+const APIPath = "api/";
 
 class Sokobot {
+  #token;
 
-    #token;
-
-    /**
-     * Creates an API instance
-     * 
-     * @param {String} token The api token gotten from https://dashboard.sokobot.info/settings/developers/api
-     */
-    constructor(token="none") {
-        
-        this.#token = token; 
-        
+  /**
+   * Creates an API instance
+   *
+   * @param {String} token The api token gotten from https://dashboard.sokobot.info/settings/developers/api
+   */
+  constructor(token = "none") {
+    if (!token) {
+      console.log("No Token Provided");
+      return null;
     }
+    this.#token = token;
+  }
 
-    /**
-     * Gets a users info like their level, balance, etc.
-     * 
-     * @param {String} userId 
-     * @returns {User}
-     * @public
-     */
-    getUser(userId) {
-        request.get(baseURL + APIPath + "users/"+userId,
-        {headers:{"Authorization": this.#token}},function (error, response, body) {
+  /**
+   * Gets a user's info like their level, balance, etc.
+   *
+   * @param {String} userId
+   * @returns {Promise<Sokobot.User>}
+   * @public
+   */
+  async getUser(userId) {
+    return new Promise((resolve, reject) => {
+      request.get(
+        baseURL + APIPath + "users/" + userId,
+        { headers: { Authorization: this.#token }, rejectUnauthorized: false },
+        async function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            let user = await Sokobot.handleCode(response, body, (r, b) => {
+              return new Sokobot.User(b);
+            });
+            resolve(user);
+          }
+        }
+      );
+    });
+  }
+  /**
+   *
+   *
+   * @param {request.Response} response
+   * @param {*} body
+   * @param {function} callback
+   * @private
+   */
+  static async handleCode(response, body, callback) {
+    await Sokobot.handleCodes(response.statusCode);
 
-
-            Sokobot.handleCode(response,body, (r,b) => {
-                return new User(b)
-            })
-            
-        })
-    }
-    /**
-     * 
-     * 
-     * @param {request.Response} response
-     * @param {*} body
-     * @param {function} callback
-     * @private
-     */
-    static handleCode(response,body, callback) {
-        let status = Sokobot.handleCodes(response.statusCode)
-        if (status!=undefined) {console.log(status); return;}
-        return callback(response,body)
-    }
-    /**
-     * @private
-     */
-    static handleCodes(code) {
-        if (code===200)return undefined
-        if (code===201) return undefined
-        if (code===401) return "Token Provided was invalid"
-        if (code===500) return "Host Down"
-        if (code===429) return "Rate Limited please wait"
-        if (code===404) return "Content doesn't exist"
-    }
+    return callback(response, body);
+  }
+  /**
+   * @private
+   */
+  static async handleCodes(code) {
+    // success codes
+    if (code === 200) return;
+    if (code === 201) return;
+    // error codes
+    if (code === 401)
+      console.error(
+        'Token Provided was invalid {code: 401, message: "The token provided is invalid"}'
+      );
+    if (code === 429)
+      console.error(
+        'Rate Limited please wait {code: 429, message: "Normally returned from cloudflare (requests/time) 25/10s"}'
+      );
+    if (code === 404) console.error("Content doesn't exist");
+    // server errors
+    if (code === 500) console.error("Host Down");
+    if (code === 501) console.error("Not Implemented");
+    process.exit();
+  }
 }
-class User {
+/**
+ *
+ * @private
+ */
+Sokobot.User = class {
+  #raw;
+  #level;
+  #inventory;
+  #balance;
+  #language;
 
-    #raw
-    #level
-    #inventory
-    #balance
-    #language
-
-    /**
-     * 
-     * @param {String} userJSON the users json sent by the api 
-     */
-    constructor(userJSON) {
-        this.#raw = userJSON
-        let json = JSON.parse(userJSON)
-        this.#level=json.level
-        this.#inventory=json.inventory
-        this.#balance=json.balance
-        this.#language="NOT IMPLEMENTED"
-        console.log(json)
-    }
-    getLevel() {
-        return this.#level
-    }
-    
-}
+  /**
+   *
+   * @param {String} userJSON the users json sent by the api
+   */
+  constructor(userJSON) {
+    this.#raw = userJSON;
+    let json = JSON.parse(userJSON);
+    this.#level = json.level;
+    this.#inventory = json.inventory;
+    this.#balance = json.balance;
+    this.#language = "NOT IMPLEMENTED";
+  }
+  /**
+   * Gets the users level
+   *
+   * @returns {int}
+   */
+  getLevel() {
+    return this.#level;
+  }
+  /**
+   * Gets the users Balance
+   *
+   * @returns {int}
+   */
+  getBalance() {
+    return this.#balance;
+  }
+  /**
+   * Gets the users level
+   *
+   * @returns {JSON}
+   */
+  getInventory() {
+    return JSON.parse(this.#inventory);
+  }
+  /**
+   * Gets the users info how it was received
+   *
+   * Can be used for your own parsing
+   *
+   * @returns {int}
+   */
+  getRaw() {
+    return this.#raw;
+  }
+  /**
+   * Gets the users Language preference
+   *
+   * @returns {String}
+   */
+  getLanguage() {
+    return this.#language;
+  }
+};
 module.exports = Sokobot;
--- testfile.js
+++ testfile.js
@@ -1,5 +1,11 @@
 const Sokobot = require("./index.js")
-let user =new Sokobot("gUJm%jAj-gTalR-JSAYj-@vUaG9-q$CgsSj9-DiEUK%ROhc7-C586%d9-H@6@l6-YslVNl-8trQJJMpPey@tHq@9-D@*6ZNJIb5").getUser("766104191403163708")
+const sokobot =  new Sokobot("gUJm%jAj-gTalR-JSAYj-@vUaG9-q$CgsSj9-DiEUK%ROhc7-C586%d9-H@6@l6-YslVNl-8trQJJMpPey@tHq@9-D@*6ZNJIb5")
+
+async function getLevel() {
+
+let user = await sokobot.getUser("766104191403163708")
+
 console.log(user.getLevel())
 
-return
+}
+getLevel()
